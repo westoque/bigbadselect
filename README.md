b0832f79c2efdde6af9616f9c05e5745ef60e4a6
William Estoque <william.estoque@gmail.com>
2013-04-03 13:39:31 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+customselect
+============
+
+A jQuery plugin for creating minimalistic custom selects. It doesn't modify
+anything about a `select` besides adding an html layer on top which can
+be easily styled. Also works inline inside a `form`. So you do not need
+to change anything to your current structure.
+
+Doesn't use themes (who uses them anyways?).
+
+Requirements
+------------
+
+jQuery version ~> 1.9.1
+
+Example
+-------
+
+```js
+$('select').selectify();
+```
+
+Creates a simple DOM structure that is easily styled. The DOM structure
+is as follows.
+
+A select with:
+
+```html
+<select>
+  <option value="CA">California</option>
+  <option value="NY">New York</option>
+</select>
+```
+
+Produces:
+
+```html
+<div class="selectify">
+  <div class="selectify-current-container">California</div>
+  <div class="selectify-options-container">
+    <div class="selectify-option selected" data-val="CA">California</div>
+    <div class="selectify-option" data-val="NY">New York</div>
+  </div>
+</div>
+```
+
+License
+-------
+
+MIT
---
+++ src/selectify.js
@@ -0,0 +1,143 @@
+(function ($, window, document) {
+
+  /**
+   * The main template
+   *
+   * @returns {string}
+   */
+  var mainTmpl = function() {
+     return '<div class="selectify">' +
+              '<div class="selectify-current-container" tabindex="-1"></div>' +
+              '<div class="selectify-options-container" tabindex="-1"></div>' +
+            '</div>';
+  };
+
+  /**
+   * The default template for the currently selected select
+   *
+   * @param val
+   * @param text
+   * @returns {String}
+   */
+  var defaultCurrentTmpl = function(val, text) {
+    return text;
+  };
+
+  /**
+   * The default template for an option
+   *
+   * @param val
+   * @param text
+   * @returns {String}
+   */
+  var defaultOptionTmpl = function(val, text) {
+    return text;
+  };
+
+
+  /**
+   * Creates a selectified select
+   *
+   * @param select
+   * @constructor
+   */
+  var Selectify = function(select, currentTmpl, optionTmpl) {
+    this.currentTmpl = currentTmpl || defaultCurrentTmpl;
+    this.optionTmpl  = optionTmpl || defaultOptionTmpl;
+    this.$el = $(mainTmpl());
+    this.$currentCont = this.$el.find('.selectify-current-container');
+    this.$optionsCont = this.$el.find('.selectify-options-container');
+    this.$select = $(select);
+    this.$select.addClass('selectified');
+    this.$selectedOption = this.$select.find(':selected');
+    this.renderCurrent();
+    this.addOptions();
+    this.bindHandlers();
+    this.injectDom();
+  };
+
+  Selectify.prototype.renderCurrent = function() {
+    this.$currentCont.html(
+      this.currentTmpl(this.$selectedOption.val(), this.$selectedOption.text())
+    );
+  };
+
+  /**
+   * Adds all the options from the select
+   */
+  Selectify.prototype.addOptions = function() {
+    this.$select.find('option').each(function(i, option) {
+      var $option  = $(option);
+      var $sOption = $('<div>');
+
+      // Add the class name
+      $sOption.addClass('selectify-option');
+
+      // Add the data-val attribute
+      $sOption.attr('data-val', $option.val());
+
+      // Render the option template
+      $sOption.html(this.optionTmpl($option.val(), $option.text()));
+
+      // Add a selected class if selected
+      if ($option.val() === this.$selectedOption.val()) {
+        $sOption.addClass('selected');
+      }
+
+      this.$optionsCont.append($sOption);
+    }.bind(this));
+  };
+
+  Selectify.prototype.bindHandlers = function() {
+    this.$currentCont.on('focus', function() {
+      this.$optionsCont.show();
+      this.$optionsCont.focus();
+    }.bind(this));
+
+    this.$optionsCont.on('blur', function() {
+      this.$optionsCont.hide();
+    }.bind(this));
+
+    this.$optionsCont.on('click', '.selectify-option', function(event) {
+      var $sOption = $(event.currentTarget);
+
+      // Hide the options
+      this.$optionsCont.hide();
+
+      // Return if the selected one is re-selected
+      if ($sOption.attr('data-val') === this.$selectedOption.val()) {
+        return;
+      }
+
+      // Find the selected class and remove the class
+      this.$optionsCont.find('.selected').removeClass('selected');
+
+      // Add the selected class to the new selectify option
+      $sOption.addClass('selected');
+
+      // Find the selected value in the select and fire a change event
+      this.$select.val($sOption.attr('data-val')).change();
+
+      // Set the newly selected option
+      this.$selectedOption = this.$select.find(':selected');
+
+      // Re-render the current
+      this.renderCurrent();
+    }.bind(this));
+  };
+
+  Selectify.prototype.injectDom = function() {
+    this.$select.after(this.$el);
+    return this;
+  };
+
+  // Main entry point
+  $.fn.selectify = function(options) {
+    options = options || {};
+    this.each(function(index, select) {
+      new Selectify(select, options.currentTmpl, options.optionsTmpl);
+    });
+    return this;
+  };
+
+})(jQuery, window, document);
